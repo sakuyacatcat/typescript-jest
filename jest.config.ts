bc6968f86bfebdce017b2c61a54dcde6b94c753c
sakuyacatcat <shingo.takagi0116@gmail.com>
2023-11-09 00:36:27 +0900

    feat: jest setup

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+import { JestConfigWithTsJest } from "ts-jest/dist/types";
+import { defaults as tsjPreset } from "ts-jest/presets";
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  verbose: true,
+  roots: ["<rootDir>"],
+  transform: {
+    ...tsjPreset.transform,
+  },
+  testMatch: ["<rootDir>/src/**/*.test.ts"],
+  modulePaths: ["<rootDir>/src"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^@test/(.*)$": "<rootDir>/test/$1",
+  },
+  moduleDirectories: ["node_modules", "<rootDir>"],
+};
+
+export default jestConfig;
